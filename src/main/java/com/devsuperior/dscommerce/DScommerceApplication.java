3e9c7ac5f07a545e280bed830e7d0a2e71862306
wrffilho <wanderlei.filho@prodam.gov.br>
2024-07-04 11:11:35 -0400

    primeiro commit

---
+++ src/main/java/com/devsuperior/dscommerce/DScommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DScommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DScommerceApplication.class, args);
+	}
+
+}
