0eabc344a8aaea4fcac7a1fccc8a72548fd393e5
wrffilho <wanderlei.filho@prodam.gov.br>
2024-07-04 14:33:54 -0400

    payment

--- src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -4,48 +4,44 @@ import java.time.Instant;
 
 import com.devsuperior.dscommerce.entities.enums.OrderStatus;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
+	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	private OrderStatus status;
 
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
+	@OneToOne
+	@MapsId
+	private Order order;
 
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
-
-	public Order() {
+	public Payment() {
 		// TODO Auto-generated constructor stub
 	}
+	
 
-	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+	public Payment(Long id, Instant moment, OrderStatus status, Order order) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
-		this.client = client;
-		this.payment = payment;
+		this.order = order;
 	}
+
 
 	public Long getId() {
 		return id;
@@ -71,20 +67,13 @@ public class Order {
 		this.status = status;
 	}
 
-	public User getClient() {
-		return client;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setClient(User client) {
-		this.client = client;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
-
-	public Payment getPayment() {
-		return payment;
-	}
-
-	public void setPayment(Payment payment) {
-		this.payment = payment;
-	}
+	
 
 }
